bac71358dc218c3f1417756825c54f0d32b67587
Josue Miguel <josmiguel92@gmail.com>
2019-05-06 17:12:37 -0400

    adding dist files and temporary __main.css, for grapejs use

--- public/dist/js/detects.js
+++
@@ -1,9 +0,0 @@
-/*!
- * Kuba-reisen v0.0.1
- * Kuba-reisen website.
- * (c) 2019 HabanaTech
- * MIT License
- * https://github.com/habana-tech/kuba-reisen.git
- */
-
-console.log('detects.js file loaded');;
