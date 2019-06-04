af7d296a8917e82eb1ff024c952e673a839714aa
landy <14ndy15@gmail.com>
2019-06-04 19:19:54 -0400

    new git ignore and responsive images

--- src/Twig/AppRuntime.php
+++ src/Twig/AppRuntime.php
@@ -46,7 +46,11 @@ class AppRuntime implements RuntimeExtensionInterface
      * @param string $class, for the html img tag
      * @return string|FilterTag[]
      */
-    public function imgTagContent($imagePath, $database64 = null,  $alt = null, $sizes = '100vw', $attr = null)
+    public function imgTagContent($imagePath,
+                                  $database64 = null,
+                                  $alt = null,
+                                  $sizes = '100vw',
+                                  $attr = null)
     {   
 
         $html ="<img class=\"lazyload blur-up\" sizes=\"$sizes\" ";
