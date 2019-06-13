a6e50fae98f833fec228e677730b23fc2bc79a7f
landy <14ndy15@gmail.com>
2019-06-13 16:54:55 -0400

    adding new map framework mapbox

--- assets/scripts/modules/activity/_map.js
+++ assets/scripts/modules/activity/_map.js
@@ -1,17 +1,18 @@
-import leaflet from 'leaflet';
+import mapboxgl from 'mapbox-gl';
 
 class ActivityMap {
     constructor(){
-        this.map = leaflet.map('mapid').setView([23.094, -82.329], 9);
-        leaflet.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox.streets',
-            accessToken: 'pk.eyJ1IjoibDRuZHkiLCJhIjoiY2p3czlqd3JxMDFsZzRhcGVta3V2ZTF5ZCJ9.LtTjS75OzU1QOxcyYgLXTA'
-        }).addTo(this.map);
-        
+        mapboxgl.accessToken = 'pk.eyJ1IjoibDRuZHkiLCJhIjoiY2p3czlqd3JxMDFsZzRhcGVta3V2ZTF5ZCJ9.LtTjS75OzU1QOxcyYgLXTA';
+        this.map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/outdoors-v11',
+            zoom: 9,
+            center: [-82.329, 23.094],
+        });
+        new mapboxgl.Marker()
+            .setLngLat([-82.329, 23.094])
+            .addTo(this.map);
     }
-
 }
 
 export default ActivityMap;
