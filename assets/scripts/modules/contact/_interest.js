8c65f62b6a53bccca82723a7d358e81b66238c46
Josue Miguel <josmiguel92@gmail.com>
2019-09-08 02:39:51 -0400

    set the start status, to avoid inconsistences on sendind info

--- assets/scripts/modules/contact/_interest.js
+++ assets/scripts/modules/contact/_interest.js
@@ -2,10 +2,24 @@ class Interest{
     constructor(){
         this.interest = document.querySelectorAll('.interest');
         this.events();
+        this.init();
     }
 
     events(){
         this.interest.forEach(x => x.addEventListener('click', this.toggleCheck.bind(this)))
+    }
+
+    init(){
+        let interests = document.querySelectorAll('.travel-interest__options > div.interest');
+        interests.forEach(function(interest){
+            let input = interest.querySelector('input[type=checkbox]');
+            if(input.checked)
+            {
+                interest.classList.add('interest--checked');
+                interest.querySelector('svg').classList.add('icon-heart--fill-green');
+            }
+            
+        })
     }
 
     toggleCheck(e){
@@ -13,6 +27,8 @@ class Interest{
         let interest = e.target.parentNode;
         interest.classList.toggle('interest--checked');
         interest.querySelector('svg').classList.toggle('icon-heart--fill-green');
+        let input = interest.querySelector('input[type=checkbox]');
+        input.checked = !input.checked;
 
     }
 }
