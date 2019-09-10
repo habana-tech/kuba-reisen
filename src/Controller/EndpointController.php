408d016b0bb1fbc1dd36ced15e16c127a7c3d40a
Josue Miguel <josmiguel92@gmail.com>
2019-09-10 18:10:22 -0400

    test email sendig

--- src/Controller/EndpointController.php
+++ src/Controller/EndpointController.php
@@ -177,4 +177,20 @@ class EndpointController extends AbstractController
             ]
         );
     }
+
+    /**
+     * @Route("/testmail")
+     */
+    public function sendEmail($name, \Swift_Mailer $mailer)
+    {
+        $message = (new \Swift_Message('Hello Email'))
+            ->setFrom('kontaktieren@kuba-reisen.reisen')
+            ->setTo('josmiguel92@gmail.com')
+            ->setBody('You should see me from the profiler!')
+        ;
+
+        $mailer->send($message);
+
+        return $this->json(['ok']);
+    }
 }
