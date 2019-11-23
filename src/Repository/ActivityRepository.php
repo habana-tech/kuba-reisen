ab4ec0c021bae834d616fe7c633a1ad578696bad
Josue Miguel <josmiguel92@gmail.com>
2019-11-23 00:25:32 -0500

    upgrade to symfony 4.4 LTS

--- src/Repository/ActivityRepository.php
+++ src/Repository/ActivityRepository.php
@@ -7,6 +7,7 @@ use App\Entity\FilterTag;
 use App\Repository\FilterTagRepository;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Activity|null find($id, $lockMode = null, $lockVersion = null)
@@ -16,7 +17,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class ActivityRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Activity::class);
     }
--- src/Repository/DynamicPageRevisionRepository.php
+++ src/Repository/DynamicPageRevisionRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\DynamicPageRevision;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method DynamicPageRevision|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +15,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class DynamicPageRevisionRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, DynamicPageRevision::class);
     }
