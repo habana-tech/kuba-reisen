abcb59ee64f6ef31d0276e7a4afc40b7b6ed4be4
Josue Miguel <josmiguel92@gmail.com>
2020-01-06 18:22:58 -0500

    fix Case error

--- src/Entity/DynamicPage.php
+++ src/Entity/DynamicPage.php
@@ -31,7 +31,7 @@ class DynamicPage implements MachineNameInterface, GalleryFieldInterface, Descri
     private $id;
 
     /**
-     * @ORM\manyToMany(targetEntity="DescriptionFragment", cascade={"persist", "remove"})
+     * @ORM\ManyToMany(targetEntity="DescriptionFragment", cascade={"persist", "remove"})
      * @ORM\JoinTable(name="page_fragments",
      *      joinColumns={@ORM\JoinColumn(name="page_id", referencedColumnName="id")},
      *      inverseJoinColumns={@ORM\JoinColumn(name="fragment_id", referencedColumnName="id", unique=true)}
