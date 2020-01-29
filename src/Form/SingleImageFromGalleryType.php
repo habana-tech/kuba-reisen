d737cb7d38595478755260b141639c33cf4002bb
Josue Miguel <josmiguel92@gmail.com>
2020-01-28 22:12:42 -0500

    adding select with images on easyadmin

---
+++ src/Form/SingleImageFromGalleryType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Image;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SingleImageFromGalleryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('image', EntityType::class, [
+                'class'=> Image::class,
+                'choice_label' => 'description',
+                'multiple' => false,
+                'attr' => [
+                    'class' => 'selectpicker',
+                ],
+                'choice_attr' => function($choice, $key, $value) {
+                    // adds a class like attending_yes, attending_no, etc
+                    dump($choice, $key, $value);
+                    return [
+                        'data-thumbnail' => $choice->getBase64(),
+//                        'data-thumbnail' => '/static/uploads/images/'.$choice->getimageName(),
+//                        'class' => "rounded-circle"
+                    ];
+                },
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Image::class,
+        ]);
+    }
+}
