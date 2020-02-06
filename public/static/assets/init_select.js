e60c93855461404874e12e7fd66c78016d2eff91
landy <14ndy15@gmail.com>
2020-02-06 17:12:04 -0500

    new thumbs size

--- public/static/assets/init_select.js
+++
@@ -1,6 +0,0 @@
-let modifiedWhiteList = $.fn.selectpicker.Constructor.DEFAULTS.whiteList;
-modifiedWhiteList.img.push('data-src');
-
-$('.selectpicker').selectpicker({
-    whiteList: modifiedWhiteList
-});
