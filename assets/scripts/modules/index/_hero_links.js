84c02b71dc4d03c227e4cf066b57b83734dd7299
Josue Miguel <josmiguel92@gmail.com>
2020-05-17 18:46:10 -0400

    fix hero links bug, on clicking headers

--- assets/scripts/modules/index/_hero_links.js
+++ assets/scripts/modules/index/_hero_links.js
@@ -1,25 +1,28 @@
 class HeroLinks {
-    constructor(){
+    constructor()
+    {
         this.links = document.querySelectorAll('.hero__buttom-links__item a');
         this.events();
     }
 
-    events(){
-        this.links.forEach((link)=>link.addEventListener('click', this.expand.bind(this)));
+    events()
+    {
+        this.links.forEach((link) => link.addEventListener('click', this.expand.bind(this)));
     }
 
-    expand(e){
-        if(e.target.href === '#') //only prevent Default if the link is an list header
-        {
-             e.preventDefault();
+    expand(e)
+    {
+        if (e.target.attributes.href.value === '#') { //only prevent Default if the link is an list header
+            e.preventDefault();
         }
 
         let activeLink = e.target;
 
-        this.links.forEach((link)=>{
+        this.links.forEach((link) => {
             let parent = link.parentNode;
-            if (parent !== activeLink.parentNode)
+            if (parent !== activeLink.parentNode) {
                 link.parentNode.classList.remove('hero__buttom-links__item--active');
+            }
         });
 
         activeLink.parentNode.classList.toggle('hero__buttom-links__item--active');
