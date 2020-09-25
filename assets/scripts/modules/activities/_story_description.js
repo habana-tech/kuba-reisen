07c5770d1def8c5c02bec1df8c87996a55f58b11
Josue Miguel <josmiguel92@gmail.com>
2020-09-25 04:00:16 -0400

    fix bug on stories at show markers on map

--- assets/scripts/modules/activities/_story_description.js
+++ assets/scripts/modules/activities/_story_description.js
@@ -2,7 +2,8 @@ import mapboxgl from 'mapbox-gl';
 
 class StoryDescription{
 
-    constructor(){
+    constructor()
+    {
 
         mapboxgl.accessToken = 'pk.eyJ1IjoiY2FydG1hbnVzZXIiLCJhIjoiY2p5aHVyNHB2MDNudzNnbnJiaGVtcWJ2OCJ9.2UPDKnSZRNwNR1ITlZQEAA';
         this.map = new mapboxgl.Map({
@@ -30,54 +31,62 @@ class StoryDescription{
         this.events();
     }
 
-    events(){
+    events()
+    {
         this.map.on('load',this.readPaths.bind(this));
 
-        this.storiesSelectors.forEach((storySelector)=>{
+        this.storiesSelectors.forEach((storySelector) => {
             storySelector.addEventListener('click', this.changeActiveStory.bind(this));
         });
 
-        window.onscroll = () => {
 
-            this.paths.forEach((path, index)=>{
-                if (this.isElementOnScreen(path)) {
-                    let bound = path.getBoundingClientRect();
-                    console.log(bound);
-                    console.log(bound.top > 0  && bound.top < window.innerHeight / 2);
-                    this.setActivePath(index);
-                }
-            });
 
-            //TODO: ugly solution
-            this.markers.forEach((marker)=>{
-                if (this.isElementOnScreen(marker)) {
-                    let bound = marker.getBoundingClientRect();
-                    console.log(bound);
-                    console.log(bound.top > 0  && bound.top < window.innerHeight / 2);
-                    let props = marker.getAttribute('data-map');
-                    props = JSON.parse(props);
-
-                    this.map.flyTo({center: props.center, zoom: props.zoom});
-                }
-            });
-        };
+            window.onscroll = () => this.iterateOverMarksAndPaths();
 
     }
 
-    readPaths(){
-        if (this.paths.length === 0)
-            return;
+    iterateOverMarksAndPaths()
+    {
+        this.paths.forEach((path, index) => {
+            if (this.isElementOnScreen(path)) {
+                console.log(path)
+                // let bound = path.getBoundingClientRect();
+                this.setActivePath(index);
+            }
+        });
 
-        this.paths.forEach((path, index)=>{
+        //TODO: ugly solution
+        this.markers.forEach((marker) => {
+            if (this.isElementOnScreen(marker)) {
+                console.log(marker)
+                let bound = marker.getBoundingClientRect();
+                // console.log(bound);
+                // console.log(bound.top > 0  && bound.top < window.innerHeight / 2);
+                let props = marker.getAttribute('data-map');
+                props = JSON.parse(props);
+
+                this.map.flyTo({center: props.center, zoom: props.zoom});
+            }
+        });
+    }
+
+
+    readPaths()
+    {
+        if (this.paths.length === 0) {
+            return;
+        }
+
+        this.paths.forEach((path, index) => {
 
             let props = path.getAttribute('data-path-map');
             props = JSON.parse(props);
             this.geojsons.push(props.geojson);
 
             let coordinates = [];
-            props.geojson.features.forEach((feature)=>{
-                if (feature.geometry.type==='LineString'){
-                    feature.geometry.coordinates.forEach((coordinate)=>{
+            props.geojson.features.forEach((feature) => {
+                if (feature.geometry.type === 'LineString') {
+                    feature.geometry.coordinates.forEach((coordinate) => {
                         coordinates.push(coordinate);
                     })
                 }
@@ -86,20 +95,20 @@ class StoryDescription{
             let lats = [];
             let logs = [];
 
-            coordinates.forEach((coordinate)=> {
+            coordinates.forEach((coordinate) => {
                 lats.push(coordinate[0]);
                 logs.push(coordinate[1]);
             });
 
-            lats.sort((a,b) => { return a <= b ? -1 : 1 } );
-            logs.sort((a,b) => { return a <= b ? -1 : 1 } );
+            lats.sort((a,b) => { return a <= b ? -1 : 1 });
+            logs.sort((a,b) => { return a <= b ? -1 : 1 });
 
             let maxCoords = [[lats[0],logs[0]],
-                [lats[lats.length-1], logs[logs.length-1] ]];
+                [lats[lats.length - 1], logs[logs.length - 1] ]];
             this.maxViewCoords.push(maxCoords);
 
             this.layers.push({
-                'id': 'layer-path_'+index,
+                'id': 'layer-path_' + index,
                 'type': 'line',
                 'source': {
                     'type': 'geojson',
@@ -119,9 +128,10 @@ class StoryDescription{
 
     }
 
-    animateMarker(timestamp) {
+    animateMarker(timestamp)
+    {
 
-        if (this.previousTime + 41 < timestamp){
+        if (this.previousTime + 41 < timestamp) {
             this.previousTime = timestamp;
 
             this.marker.setLngLat(this.geojson.features[0].geometry.coordinates[this.pos]);
@@ -130,51 +140,65 @@ class StoryDescription{
 
             this.pos += 1;
 
-            if (this.pos === this.geojson.features[0].geometry.coordinates.length)
+            if (this.pos === this.geojson.features[0].geometry.coordinates.length) {
                 this.pos = 0;
+            }
         }
 
         requestAnimationFrame(this.animateMarker.bind(this));
     }
 
-    changeActiveStory(e) {
+    changeActiveStory(e)
+    {
         let storySelector = e.target;
 
+        console.log('Story changed')
         let storyId = storySelector.getAttribute('data-story');
         let associateStory = document.querySelector(storyId);
         this.paths = associateStory.querySelectorAll('span[data-path-map]');
+        this.markers = associateStory.querySelectorAll('span[data-map]');
         this.removeLayers([...Array(12).keys()]);
         this.readPaths();
+        // this.events();
     }
 
-    removeLayers(indexs){
-        indexs.forEach((index)=>{
-            let previousLayerId = 'layer-path_'+index;
+    removeLayers(indexs)
+    {
+        indexs.forEach((index) => {
+            let previousLayerId = 'layer-path_' + index;
             let previousMapLayer = this.map.getLayer(previousLayerId);
             let previousMapSource = this.map.getSource(previousLayerId);
-            if(typeof previousMapLayer !== 'undefined')
+            if (typeof previousMapLayer !== 'undefined') {
                 this.map.removeLayer(previousLayerId);
-            if(typeof previousMapSource !== 'undefined')
+            }
+            if (typeof previousMapSource !== 'undefined') {
                 this.map.removeSource(previousLayerId);
+            }
         });
     }
 
-    setActivePath(index){
+    setActivePath(index)
+    {
 
-        this.removeLayers([index-1, index+1]);
+        this.removeLayers([index - 1, index + 1]);
 
-        let currentLayerId = 'layer-path_'+index;
+        let currentLayerId = 'layer-path_' + index;
         let currentMapLayer = this.map.getLayer(currentLayerId);
 
-        if(typeof currentMapLayer === 'undefined') {
+        if (typeof currentMapLayer === 'undefined') {
             this.map.addLayer(this.layers[index]);
             this.map.fitBounds(this.maxViewCoords[index], {padding: 20});
         }
 
     }
 
-    isElementOnScreen(element) {
-        element = element.parentNode.parentNode.parentNode.parentNode;
+    isElementOnScreen(element)
+    {
+        console.log('current marker: ', element)
+        while (element.parentNode.classList.contains('activities__stories__story__item__content') === false) {
+            element = element.parentNode;
+        }
+        console.log('parent node of current marker', element)
         let bounds = element.getBoundingClientRect();
         return bounds.top > 0  && bounds.top < window.innerHeight / 2;
     }
